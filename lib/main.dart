1a922f3c248264595be358fc937592ac38cad333
Mohamed <mh_jo.2015@hotmail.com>
2022-07-20 16:18:39 +0300

    commit master

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
       ],
       debugShowCheckedModeBanner: false,
       title: 'Smart Service',
-      home:  MainScreen(),
+      home:  ScreenShot(),
     );
 
   }
