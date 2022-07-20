3afae150b3ad69c9d1a2b45c83c98a6d90017b71
Mohamed <mh_jo.2015@hotmail.com>
2022-07-20 15:35:31 +0300

    commit master

---
+++ lib/Home/Details_Service/Details_Service.dart
@@ -0,0 +1,267 @@
+import 'package:flutter/material.dart';
+import 'package:smart_service/Home/Home_Screen.dart';
+import 'package:smart_service/constant.dart';
+import 'package:unicons/unicons.dart';
+
+class DetailsService extends StatelessWidget {
+  const DetailsService({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      // appBar: AppBar(
+      //   elevation: 0,
+      //   backgroundColor: const Color(0xffF6C52F),
+      //   title: const Text(
+      //     'خدمة صيانة المنازل',
+      //     style: TextStyle(
+      //       fontFamily: 'Tajawal',
+      //       fontSize: 20,
+      //       color: const Color(0xff0f0a39),
+      //       fontWeight: FontWeight.w700,
+      //       height: 0.6,
+      //     ),
+      //     textHeightBehavior:
+      //         TextHeightBehavior(applyHeightToFirstAscent: false),
+      //     textAlign: TextAlign.right,
+      //     softWrap: false,
+      //   ),
+      //   actions: [
+      //     IconButton(
+      //       onPressed: () {},
+      //       icon: const Icon(
+      //         Icons.search,
+      //       ),
+      //     ),
+      //   ],
+      //   iconTheme: const IconThemeData(
+      //     color: Color(0xff0F0A39),
+      //   ),
+      // ),
+      body: Column(
+        children: [
+          Expanded(
+            child: Stack(
+              children: [
+                Container(
+                  height: size.height * 0.3,
+                  width: double.infinity,
+                  decoration: const BoxDecoration(
+                    color: Color(0xffF6C52F),
+                    borderRadius: BorderRadius.only(
+                      bottomRight: Radius.circular(
+                        40,
+                      ),
+                      bottomLeft: Radius.circular(
+                        40,
+                      ),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Stack(
+                      children: [
+                        Positioned(
+                          top: size.height * 0.08,
+                          right: size.width * 0.05,
+                          child: IconButton(
+                            onPressed: () {
+                              navigatorFinished(context, const HomeScreen());
+                            },
+                            icon: const Icon(
+                              Icons.arrow_back_ios,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: size.height * 0.1,
+                          right: size.width * 0.2,
+                          child: const Text(
+                            'خدمة صيانة المنازل',
+                            style: TextStyle(
+                              fontFamily: 'Tajawal',
+                              fontSize: 20,
+                              color: const Color(0xff0f0a39),
+                              fontWeight: FontWeight.w700,
+                              height: 0.6,
+                            ),
+                            textHeightBehavior: TextHeightBehavior(
+                                applyHeightToFirstAscent: false),
+                            textAlign: TextAlign.right,
+                            softWrap: false,
+                          ),
+                        ),
+                        Positioned(
+                          top: size.height * 0.08,
+                          left: size.width * 0.02,
+                          child: IconButton(
+                            onPressed: () {
+                              navigatorFinished(context, const HomeScreen());
+                            },
+                            icon: const Icon(
+                              UniconsLine.search,
+                            ),
+                          ),
+                        ),
+                        Positioned(
+                          top: size.height * 0.15,
+                          right: size.width * 0.08,
+                          child: Container(
+                            height: size.height * 0.035,
+                            width: size.width * 0.2,
+                            decoration: BoxDecoration(
+                                color: const Color(0xffE0E0E0),
+                                borderRadius: BorderRadius.circular(10)),
+                            child: const Center(
+                              child: Text(
+                                'كهرباء',
+                                style: TextStyle(
+                                  fontFamily: 'Tajawal',
+                                  fontSize: 14,
+                                  color: Color(0xff5300bf),
+                                  letterSpacing: -0.33764714050292965,
+                                ),
+                                textAlign: TextAlign.center,
+                                softWrap: false,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * 0.24,
+                  right: size.width * 0.1,
+                  left: size.width * 0.1,
+                  child: Container(
+                    height: size.height * 0.2,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(
+                        20,
+                      ),
+                      color: const Color(0xffffffff),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(10.0),
+                          child: Row(
+                            children: [
+                              const Text(
+                                'كهرباء',
+                                style: TextStyle(
+                                  fontFamily: 'Tajawal',
+                                  fontSize: 21,
+                                  color: const Color(0xff383838),
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.2380952380952381,
+                                ),
+                                textHeightBehavior: TextHeightBehavior(
+                                    applyHeightToFirstAscent: false),
+                                textAlign: TextAlign.right,
+                                softWrap: false,
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon: Icon(
+                                  UniconsLine.circle,
+                                  color: Color(0xff707070),
+                                ),
+                              )
+                            ],
+                          ),
+                        ),
+                        const Divider(
+                          color: Color(0xffBDC4CC),
+                          height: 0.5,
+                        ),
+                        const Padding(
+                          padding: EdgeInsets.all(10.0),
+                          child: Text(
+                            'اصلاح أعطال الطبلون',
+                            style: TextStyle(
+                              fontFamily: 'Tajawal',
+                              fontSize: 16,
+                              color: Color(0xff383838),
+                              height: 1.625,
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                          child: Row(
+                            children: [
+                              const Text(
+                                '350-200',
+                                style: TextStyle(
+                                  fontFamily: 'Tajawal',
+                                  fontSize: 12,
+                                  color: const Color(0xff000000),
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.9166666666666667,
+                                ),
+                                textHeightBehavior: TextHeightBehavior(
+                                    applyHeightToFirstAscent: false),
+                                textAlign: TextAlign.right,
+                              ),
+                              const Text(
+                                'شيكل',
+                                style: TextStyle(
+                                  fontFamily: 'Tajawal',
+                                  fontSize: 12,
+                                  color: const Color(0xff000000),
+                                  fontWeight: FontWeight.w700,
+                                  height: 1.9166666666666667,
+                                ),
+                                textHeightBehavior: TextHeightBehavior(
+                                    applyHeightToFirstAscent: false),
+                                textAlign: TextAlign.right,
+                              ),
+                              const Spacer(),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  UniconsLine.minus_circle,
+                                  color: Color(0xff5300BF),
+                                  size: 25,
+                                ),
+                              ),
+                              Text(
+                                '0',
+                                style: TextStyle(
+                                  fontFamily: 'Tajawal',
+                                  fontSize: 20,
+                                  color: const Color(0xff000000),
+                                  fontWeight: FontWeight.w700,
+                                  height: 2.5,
+                                ),
+                              ),
+                              IconButton(
+                                onPressed: () {},
+                                icon: const Icon(
+                                  UniconsLine.plus_circle,
+                                  size: 25,
+                                  color: Color(0xff5300BF),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/Home/Home_Screen.dart
+++ lib/Home/Home_Screen.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:smart_service/Home/Details_Service/Details_Service.dart';
+import 'package:smart_service/constant.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -6,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var size = MediaQuery.of(context).size;
+    var searchController = TextEditingController();
     return Scaffold(
       body: Column(
         children: [
@@ -28,7 +33,7 @@ class HomeScreen extends StatelessWidget {
                 ),
                 Positioned(
                   top: size.height * 0.17,
-                  right: size.width * 0.1,
+                  right: size.width * 0.08,
                   child: const Text(
                     'مرحباً, منال منصور',
                     style: TextStyle(
@@ -39,13 +44,13 @@ class HomeScreen extends StatelessWidget {
                       height: 2.888888888888889,
                     ),
                     textHeightBehavior:
-                    TextHeightBehavior(applyHeightToFirstAscent: false),
+                        TextHeightBehavior(applyHeightToFirstAscent: false),
                     textAlign: TextAlign.right,
                   ),
                 ),
                 Positioned(
                   top: size.height * 0.2,
-                  right: size.width * 0.1,
+                  right: size.width * 0.08,
                   child: const Text(
                     'كيف يمكننا مساعدتك؟',
                     style: TextStyle(
@@ -55,31 +60,178 @@ class HomeScreen extends StatelessWidget {
                       height: 2.857142857142857,
                     ),
                     textHeightBehavior:
-                    TextHeightBehavior(applyHeightToFirstAscent: false),
+                        TextHeightBehavior(applyHeightToFirstAscent: false),
                     textAlign: TextAlign.right,
                   ),
                 ),
                 Positioned(
                   top: size.height * 0.1,
-                  right: size.width * 0.1,
-                  left: size.width * 0.1,
+                  right: size.width * 0.08,
+                  left: size.width * 0.08,
                   child: Container(
                     height: size.height * 0.05,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10,),
-                      color: const  Color(0xffffffff),
+                    decoration: const BoxDecoration(
+                      color: Color(0xffF6C52F),
+                      boxShadow: [
+                        BoxShadow(
+                          offset: Offset(0, 1),
+                          color: Color(0xff5300BF),
+                        ),
+                      ],
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        children: [
+                          const Icon(
+                            Icons.location_on,
+                            color: Color(0xff5300BF),
+                            size: 30,
+                          ),
+                          SizedBox(
+                            width: size.width * 0.03,
+                          ),
+                          const Text(
+                            'حدد موقعك',
+                            style: TextStyle(
+                              fontFamily: 'Tajawal',
+                              fontSize: 16,
+                              color: const Color(0xff000000),
+                              height: 0.1,
+                            ),
+                            textHeightBehavior: TextHeightBehavior(
+                                applyHeightToFirstAscent: false),
+                            textAlign: TextAlign.center,
+                            softWrap: false,
+                          ),
+                          const Spacer(),
+                          const Icon(
+                            Icons.keyboard_arrow_down_sharp,
+                            color: Color(0xff5300BF),
+                            size: 30,
+                          ),
+                        ],
+                      ),
                     ),
                   ),
                 ),
                 Positioned(
                   top: size.height * 0.24,
-                  right: size.width * 0.1,
-                  left: size.width * 0.1,
+                  right: size.width * 0.08,
+                  left: size.width * 0.08,
                   child: Container(
                     height: size.height * 0.05,
                     decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10,),
-                      color: const  Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(
+                        10,
+                      ),
+                      color: const Color(0xffffffff),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8),
+                      child: TextFormField(
+                        onTap: () {},
+                        textAlign: TextAlign.start,
+                        controller: searchController,
+                        decoration: const InputDecoration(
+                          border: InputBorder.none,
+                          hintText: 'ما الخدمة التي تبحث عنها',
+                          hintStyle: TextStyle(
+                            fontFamily: 'Tajawal',
+                            fontSize: 16,
+                            color: Color(0xffcbc9d9),
+                            height: 0.5,
+                          ),
+                          prefixIcon: Icon(
+                            Icons.search_sharp,
+                            color: Color(0xffcbc9d9),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Positioned(
+                  top: size.height * 0.32,
+                  right: size.width * 0.08,
+                  left: size.width * 0.08,
+                  child: Container(
+                    height: size.height * 0.1,
+                    decoration: BoxDecoration(
+                      color: const Color(0xffffffff),
+                      borderRadius: BorderRadius.circular(
+                        20,
+                      ),
+                    ),
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: ()
+                        {
+                          navigatorTo(context, DetailsService());
+                        },
+                        child: Row(
+                          children: [
+                            CircleAvatar(
+                              radius: 30,
+                              backgroundColor: const Color(0xff5300BF),
+                              child: Image(
+                                image: const AssetImage(
+                                  'assets/images/icon.png',
+                                ),
+                                fit: BoxFit.contain,
+                                height: size.height * 0.035,
+                                width: size.width * 0.08,
+                              ),
+                            ),
+                            SizedBox(
+                              width: size.width * 0.03,
+                            ),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                const Text(
+                                  'خدمة صيانة المنازل',
+                                  style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    fontSize: 16,
+                                    color: Color(0xff383838),
+                                    fontWeight: FontWeight.w700,
+                                    height: 1.625,
+                                  ),
+                                  textHeightBehavior: TextHeightBehavior(
+                                      applyHeightToFirstAscent: false),
+                                  textAlign: TextAlign.right,
+                                  softWrap: false,
+                                ),
+                                SizedBox(
+                                  height: size.height * 0.01,
+                                ),
+                                const Text(
+                                  'تفاصيل عن الخدمة',
+                                  style: TextStyle(
+                                    fontFamily: 'Tajawal',
+                                    fontSize: 14,
+                                    color: Color(0xa8000000),
+                                    height: 1.6428571428571428,
+                                  ),
+                                  textHeightBehavior: TextHeightBehavior(
+                                      applyHeightToFirstAscent: false),
+                                  textAlign: TextAlign.right,
+                                ),
+                              ],
+                            ),
+                            const Spacer(),
+                            const Icon(
+                              Icons.arrow_forward_ios_outlined,
+                              color: Color(
+                                0xffCBC9D9,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
                     ),
                   ),
                 ),
