1e86e0e9aba9600e77283f52cc6867863aca730a
Mohamed <mh_jo.2015@hotmail.com>
2022-07-25 14:49:53 +0300

    commit master

--- lib/layout/Logout_Screen.dart
+++ lib/layout/Logout_Screen.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:smart_service/modules/Login/cubit/cubit.dart';
+import 'package:smart_service/modules/Login/cubit/state.dart';
 
 class LogoutScreen extends StatelessWidget {
   const LogoutScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        children: [
-          Text('its Logout Screen'),
-        ],
+    return BlocProvider(
+      create: (context) => LoginCubit(),
+      child: BlocConsumer<LoginCubit,LoginState>(
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(),
+            body: Column(
+              children: [
+                Text('its Logout Screen'),
+              ],
+            ),
+          );
+        },
+        listener: (context, state) {},
       ),
     );
   }
